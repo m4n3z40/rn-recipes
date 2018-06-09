2b85d557bd60f5e87bac95ad884769c1d2335023
Allan Baptista <allan.marques@ymail.com>
2018-06-09 16:55:32 -0300

    Adding redux

--- config.js
+++
@@ -1,2 +0,0 @@
-// Get API KEY at http://food2fork.com/about/api
-export const food2ForkKey = 'API_KEY_HERE';
---
+++ src/config.js
@@ -0,0 +1,2 @@
+// Get API KEY at http://food2fork.com/about/api
+export const food2ForkKey = 'c91f624426f79efd26304f6950f79173';
